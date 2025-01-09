693363150164052df082a0b698514c9e946e48e9
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-09 16:26:30 +0200

    feat: add new controller

---
+++ src/videos/updateVideoController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { db } from '../db/db';
+import { InputVideoType } from '../input-output-types/video-types';
+import { OutputErrorsType } from '../input-output-types/output-errors-type';
+
+export const updateVideoController = (
+  req: Request<{ id: string }, any, Partial<InputVideoType>>,
+  res: Response<OutputErrorsType>,
+) => {
+  const videoId = parseFloat(req.params.id); // Safely parse the ID as float
+
+  if (isNaN(videoId)) {
+    res.status(400).send({ errorsMessages: [{ message: 'Invalid video ID', field: '' }] });
+    return;
+  }
+
+  const foundVideoIndex = db.videos.findIndex((video) => video.id === Number(videoId));
+
+  if (foundVideoIndex < 0) {
+    res.status(404).send();
+    return;
+  }
+
+  const foundVideo = db.videos[foundVideoIndex];
+
+  db.videos[foundVideoIndex] = { ...foundVideo, ...req.body };
+  res.status(204).send();
+};
