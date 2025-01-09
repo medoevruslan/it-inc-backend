8ca22300e6310ddcf05b151b2b94c3bdcc807c27
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-09 13:05:39 +0200

    refac: change id type

--- src/db/video-db-type.ts
+++ src/db/video-db-type.ts
@@ -2,7 +2,7 @@ import { Resolutions } from '../input-output-types/video-types';
 import { Nullable } from '../shared/types';
 
 export type VideoDBType = {
-  id: string;
+  id: number;
   title: string;
   author: string;
   canBeDownloaded: boolean;
--- src/videos/findVideoController.ts
+++ src/videos/findVideoController.ts
@@ -5,7 +5,7 @@ import { OutputVideoType } from '../input-output-types/video-types';
 export const findVideoController = (req: Request<{ id: string }>, res: Response<OutputVideoType>) => {
   const videoId = req.params.id;
 
-  const foundVideo = db.videos.find((video) => video.id === videoId);
+  const foundVideo = db.videos.find((video) => video.id === Number(videoId));
 
   if (!foundVideo) {
     res.status(404).send();
