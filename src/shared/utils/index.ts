dad018743301c323b1b54ab8f125721cf06750f1
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-09 13:00:26 +0200

    feat: create util

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,3 +1,8 @@
 export const generateId = () => {
-  return Date.now().toString(36) + Math.random().toString(36).substring(2)
-}
+  return Date.now().toString(36) + Math.random().toString(36).substring(2);
+};
+
+export const addDayTo = (date: Date) => {
+  const day = 60 * 60 * 24 * 1000;
+  return new Date(date.getTime() + day);
+};
