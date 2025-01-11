f779f8a5def7ad26cc2a2f6d5652bf12d8b20b9b
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-11 13:12:17 +0200

    feat: add paths

--- src/settings.ts
+++ src/settings.ts
@@ -1,12 +1,14 @@
 import { config } from 'dotenv';
 config();
 
-const BASE_URL = '/hometask_01/api';
+const BASE_URL = '/ht_02/api';
 
 export const SETTINGS = {
   PORT: process.env.PORT || 3003,
   PATH: {
     VIDEOS: `${BASE_URL}/videos`,
+    BLOGS: `${BASE_URL}/blogs`,
+    POSTS: `${BASE_URL}/posts`,
     TESTING: `${BASE_URL}/testing/all-data`,
   },
 };
