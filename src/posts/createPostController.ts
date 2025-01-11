12e9b3ea1a4d495528dc2906b95f0c964637a8d2
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-11 13:44:13 +0200

    feat: create post controller

--- src/posts/createPostController.ts
+++ src/posts/createPostController.ts
@@ -1,3 +1,22 @@
 import { Request, Response } from 'express';
 import { BlogDbType } from '../db/blog-db-type';
-export const createPostController = (req: Request<{}, BlogDbType>, res: Response) => {};
+import { PostDbType } from '../db/post-db.type';
+import { PostInputType } from '../input-output-types/post-types';
+import { generateIdString } from '../shared/utils';
+import { db } from '../db/db';
+export const createPostController = (req: Request<{}, PostInputType>, res: Response) => {
+  const { body } = req;
+
+  const newPost: PostDbType = {
+    id: generateIdString(),
+    content: body.content,
+    blogId: body.blogId,
+    title: body.title,
+    shortDescription: body.shortDescription,
+    blogName: body?.blogName ?? null,
+  };
+
+  db.posts.push(newPost);
+
+  res.status(201).send(newPost);
+};
