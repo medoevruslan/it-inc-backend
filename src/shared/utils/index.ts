2cac596f2f011a184ea57797be04e95fa84cd831
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-11 12:12:38 +0200

    feat: add new function

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -2,6 +2,10 @@ export const generateId = () => {
   return Date.now() + Math.random();
 };
 
+export const generateIdString = () => {
+  return Date.now().toString(36) + Math.random().toString(36).substring(2);
+};
+
 export const addDayTo = (date: Date) => {
   const day = 60 * 60 * 24 * 1000;
   return new Date(date.getTime() + day);
