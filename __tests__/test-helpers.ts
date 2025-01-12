cdb3099259ddba863d6cbdc805dc190765a4829d
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-12 17:48:40 +0200

    feat: add util

--- __tests__/test-helpers.ts
+++ __tests__/test-helpers.ts
@@ -1,5 +1,10 @@
-import {app} from '../src/app'
-import {agent} from 'supertest'
+import { app } from '../src/app';
+import { agent } from 'supertest';
+import { SETTINGS } from '../src/settings';
 
-export const req = agent(app)
+export const req = agent(app);
 
+export const toBase64 = (authString: string) => {
+  const buffer = Buffer.from(authString, 'utf-8');
+  return buffer.toString('base64');
+};
