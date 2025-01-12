b854f1d815889cf0e979eb3d28dcd0d104e20cf6
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-12 15:56:28 +0200

    feat: add add controllers to routes

--- src/blogs/index.ts
+++ src/blogs/index.ts
@@ -3,10 +3,12 @@ import { getBlogsController } from './getBlogsController';
 import { createBlogController } from './createBlogController';
 import { updateBlogController } from './updateBlogController';
 import { deleteBlogController } from './deleteBlogController';
+import { blogBodyValidator } from '../validation/blogBodyValidator';
+import { validationErrorMiddleware } from '../middlewares/validationErrorMiddleware';
 
 export const blogsRouter = Router();
 
 blogsRouter.get('/', getBlogsController);
-blogsRouter.post('/', createBlogController);
-blogsRouter.put('/:blogId', updateBlogController);
+blogsRouter.post('/', blogBodyValidator, validationErrorMiddleware, createBlogController);
+blogsRouter.put('/:blogId', blogBodyValidator, validationErrorMiddleware, updateBlogController);
 blogsRouter.delete('/:blogId', deleteBlogController);
