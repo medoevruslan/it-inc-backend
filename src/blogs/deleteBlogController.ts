199c186d50c85b0c9e8bb719dde68e7339f3e1a1
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-12 15:55:51 +0200

    feat: implement blog controllers logic

--- src/blogs/deleteBlogController.ts
+++ src/blogs/deleteBlogController.ts
@@ -1,2 +1,15 @@
 import { Request, Response } from 'express';
-export const deleteBlogController = (req: Request, res: Response) => {};
+import { db } from '../db/db';
+export const deleteBlogController = (req: Request<{ blogId: string }>, res: Response) => {
+  const blogId = req.params.blogId;
+
+  const foundIndex = db.blogs.findIndex((blog) => blog.id === blogId);
+
+  if (foundIndex < 0) {
+    res.status(404).send();
+    return;
+  }
+
+  db.blogs.splice(foundIndex, 1);
+  res.status(204).send();
+};
--- src/blogs/updateBlogController.ts
+++ src/blogs/updateBlogController.ts
@@ -1,2 +1,17 @@
 import { Request, Response } from 'express';
-export const updateBlogController = (req: Request, res: Response) => {};
+import { db } from '../db/db';
+import { BlogInputType } from '../input-output-types/blog-types';
+
+export const updateBlogController = (req: Request<{ blogId: string }, {}, BlogInputType>, res: Response) => {
+  const blogId = req.params.blogId;
+
+  const foundIndex = db.blogs.findIndex((blog) => blog.id === blogId);
+
+  if (foundIndex < 0) {
+    res.status(404).send();
+    return;
+  }
+
+  db.blogs[foundIndex] = { id: blogId, ...req.body };
+  res.status(204).send();
+};
