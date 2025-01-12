da1dbe8de14adffe11ab8365c7e2df3d7cbffcff
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-12 15:55:09 +0200

    feat: add blog validation

---
+++ src/validation/blogBodyValidator.ts
@@ -0,0 +1,26 @@
+import { body } from 'express-validator';
+
+export const blogBodyValidator = [
+  body('name')
+    .isString()
+    .withMessage('[name] should be a string')
+    .trim()
+    .notEmpty()
+    .withMessage('required')
+    .isLength({ max: 15 })
+    .withMessage('name should be less than 15 chars'),
+  body('description')
+    .isString()
+    .withMessage('[description] should be a string')
+    .trim()
+    .notEmpty()
+    .withMessage('required')
+    .isLength({ max: 500 })
+    .withMessage('description should be less than 500 chars'),
+  body('websiteUrl')
+    .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
+    .withMessage('websiteUrl should be a proper url')
+    .trim()
+    .isLength({ max: 500 })
+    .withMessage('websiteUrl should be less than 100 chars'),
+];
