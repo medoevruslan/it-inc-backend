67ed92b0da2aa85d106d1e220270306e355a9bd5
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-12 14:36:44 +0200

    feat: add conterollers to routes

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -3,10 +3,12 @@ import { getPostsController } from './getPostsController';
 import { createPostController } from './createPostController';
 import { updatePostController } from './updatePostController';
 import { deletePostController } from './deletePostController';
+import { postBodyValidator } from '../validation/postBodyValidator';
+import { validationErrorMiddleware } from '../middlewares/validationErrorMiddleware';
 
 export const postsRouter = Router();
 
 postsRouter.get('/', getPostsController);
-postsRouter.post('/', createPostController);
-postsRouter.put('/:postId', updatePostController);
-postsRouter.get('/:postId', deletePostController);
+postsRouter.post('/', postBodyValidator, validationErrorMiddleware, createPostController);
+postsRouter.put('/:postId', postBodyValidator, validationErrorMiddleware, updatePostController);
+postsRouter.delete('/:postId', deletePostController);
