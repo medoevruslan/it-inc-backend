6a678d057fb4861158b9c59013bf11ed5d910a85
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-12 23:25:07 +0200

    feat: add repository

---
+++ src/repository/postRepository.ts
@@ -0,0 +1,45 @@
+import { db } from '../db/db';
+import { generateIdString } from '../shared/utils';
+import { InputPostType, OutputPostType } from '../input-output-types/post-types';
+import { PostDbType } from '../db/post-db.type';
+
+type UpdatePostType = { postId: string; update: InputPostType };
+
+export const postRepository = {
+  async create(input: InputPostType): Promise<OutputPostType> {
+    const newPost: PostDbType = {
+      id: generateIdString(),
+      content: input.content,
+      blogId: input.blogId,
+      title: input.title,
+      shortDescription: input.shortDescription,
+      blogName: input?.blogName ?? null,
+    };
+
+    db.posts.push(newPost);
+
+    return newPost;
+  },
+  async update({ postId, update }: UpdatePostType): Promise<boolean> {
+    const foundIndex = db.posts.findIndex((post) => post.id === postId);
+    if (foundIndex < 0) {
+      return false;
+    }
+    db.posts[foundIndex] = { id: postId, ...update };
+    return true;
+  },
+  async findAll(): Promise<OutputPostType[]> {
+    return db.posts;
+  },
+  async findById(id: string): Promise<OutputPostType | null> {
+    return db.posts.find((post) => post.id === id) || null;
+  },
+  async deleteById(id: string): Promise<boolean> {
+    const foundIndex = db.posts.findIndex((post) => post.id === id);
+    if (foundIndex < 0) {
+      return false;
+    }
+    db.posts.splice(foundIndex, 1);
+    return true;
+  },
+};
