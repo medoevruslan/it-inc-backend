ce9e3d161562cd7f40302171db24b43fc691885e
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-12 14:39:43 +0200

    feat: implement controllers logic

--- src/blogs/createBlogController.ts
+++ src/blogs/createBlogController.ts
@@ -1,3 +1,12 @@
 import { Request, Response } from 'express';
 import { BlogDbType } from '../db/blog-db-type';
-export const createBlogController = (req: Request<{}, BlogDbType>, res: Response) => {};
+import { BlogInputType } from '../input-output-types/blog-types';
+import { generateIdString } from '../shared/utils';
+import { db } from '../db/db';
+export const createBlogController = (req: Request<{}, {}, BlogInputType>, res: Response<BlogDbType>) => {
+  const newBlog: BlogDbType = { id: generateIdString(), ...req.body };
+
+  db.blogs.push(newBlog);
+
+  res.status(201).send(newBlog);
+};
