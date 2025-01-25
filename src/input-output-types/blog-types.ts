e2944a0b692dae081864ff3a70f72f0d2da30229
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-25 15:25:50 +0200

    refac: change types

--- src/input-output-types/blog-types.ts
+++ src/input-output-types/blog-types.ts
@@ -1,4 +1,8 @@
 import { BlogDbType } from '../db/blog-db-type';
+import { OptionalUnlessRequiredId } from 'mongodb';
 
-export type InputBlogType = Omit<BlogDbType, 'id'>;
-export type OutputBlogType = BlogDbType;
+export type InputBlogType = OptionalUnlessRequiredId<BlogDbType>;
+export type UpdateBlogType = Omit<InputBlogType, '_id'>;
+
+// Represents the output when sending data to client
+export type OutputBlogType = Omit<BlogDbType, '_id'> & { id: string };
--- src/input-output-types/post-types.ts
+++ src/input-output-types/post-types.ts
@@ -1,5 +1,9 @@
 import { PostDbType } from '../db/post-db.type';
 import { Nullable } from '../shared/types';
+import { OptionalUnlessRequiredId } from 'mongodb';
 
-export type InputPostType = Omit<PostDbType, 'id' | 'blogName'>;
-export type OutputPostType = PostDbType;
+export type InputPostType = OptionalUnlessRequiredId<Omit<PostDbType, 'blogName'>>;
+export type UpdatePostType = Omit<InputPostType, '_id'>;
+
+// Represents the output when sending data to client
+export type OutputPostType = Omit<PostDbType, '_id'> & { id: string };
