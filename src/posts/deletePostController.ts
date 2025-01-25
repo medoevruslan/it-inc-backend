bfed85d901d5e77c41759272495cda12b9f12ed6
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-25 17:45:29 +0200

    refac: refactor controllers

--- src/posts/deletePostController.ts
+++ src/posts/deletePostController.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express';
 import { postRepository } from '../repository';
+import { ObjectId } from 'mongodb';
 
 export const deletePostController = async (req: Request<{ id: string }>, res: Response) => {
   const postId = req.params.id;
+
+  if (!ObjectId.isValid(postId)) {
+    res.status(400).send();
+    return;
+  }
 
   const success = await postRepository.deleteById(postId);
 
--- src/posts/getPostByIdController.ts
+++ src/posts/getPostByIdController.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from 'express';
 import { OutputPostType } from '../input-output-types/post-types';
 import { postRepository } from '../repository';
+import { ObjectId } from 'mongodb';
 
 export const getPostByIdController = async (req: Request<{ id: string }>, res: Response<OutputPostType>) => {
-  const found = await postRepository.findById(req.params.id);
+  const postId = req.params.id;
+
+  if (!ObjectId.isValid(postId)) {
+    res.status(400).send();
+    return;
+  }
+
+  const found = await postRepository.findById(postId);
 
   if (!found) {
     res.status(404).send();
