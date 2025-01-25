2ea0822af9cfa2d12aedbe5e553253e124c31bc5
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-25 15:20:30 +0200

    chore: add mongodb

--- src/blogs/createBlogController.ts
+++ src/blogs/createBlogController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { BlogDbType } from '../db/blog-db-type';
 import { InputBlogType } from '../input-output-types/blog-types';
 import { blogRepository } from '../repository';
-export const createBlogController = async (req: Request<{}, {}, InputBlogType>, res: Response<BlogDbType>) => {
+export const createBlogController = async (req: Request<{}, {}, InputBlogType>, res: Response<string>) => {
   const created = await blogRepository.create(req.body);
   res.status(201).send(created);
 };
