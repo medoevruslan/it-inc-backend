624d275c4a5bff955ccc6806a99d76c478ef5d82
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-26 12:48:12 +0200

    refac: change return data

--- src/blogs/createBlogController.ts
+++ src/blogs/createBlogController.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from 'express';
-import { InputBlogType } from '../input-output-types/blog-types';
+import { InputBlogType, OutputBlogType } from '../input-output-types/blog-types';
 import { blogRepository } from '../repository';
 
-export const createBlogController = async (req: Request<{}, {}, InputBlogType>, res: Response<{ id: string }>) => {
-  const created = await blogRepository.create({
+export const createBlogController = async (req: Request<{}, {}, InputBlogType>, res: Response<OutputBlogType>) => {
+  const createdId = await blogRepository.create({
     ...req.body,
     createdAt: new Date().toISOString(),
     isMembership: false,
   });
-  res.status(201).send({ id: created });
+
+  const createdBlog = await blogRepository.findById(createdId);
+
+  if (!createdBlog) {
+    res.status(400).send();
+    return;
+  }
+
+  res.status(201).send(createdBlog);
 };
