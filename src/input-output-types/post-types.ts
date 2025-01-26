f43af0034907a3409eb182156558ed825f7c3608
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-26 22:48:11 +0200

    refac: refactor type

--- src/input-output-types/post-types.ts
+++ src/input-output-types/post-types.ts
@@ -3,7 +3,7 @@ import { Nullable } from '../shared/types';
 import { OptionalUnlessRequiredId } from 'mongodb';
 
 export type InputPostType = OptionalUnlessRequiredId<Omit<PostDbType, 'blogName'>>;
-export type UpdatePostType = Omit<InputPostType, '_id'>;
+export type UpdatePostType = { postId: string; update: InputPostType };
 
 // Represents the output when sending data to client
 export type OutputPostType = Omit<PostDbType, '_id'> & { id: string };
