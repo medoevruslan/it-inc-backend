8f79fca8bf4ea7f9dee82eb34aa22bae3b7ca398
MedoevRuslan <mr.r.m3@icloud.com>
2025-01-29 00:36:30 +0200

    feat: create service

--- src/service/blogService.ts
+++ src/service/blogService.ts
@@ -1,1 +1,61 @@
-export const blogService = {};
+import { InputBlogType, OutputBlogType, UpdateBlogType } from '../input-output-types/blog-types';
+import { ObjectId } from 'mongodb';
+import { blogRepository } from '../repository';
+
+export const blogService = {
+  async create(input: InputBlogType): Promise<OutputBlogType> {
+    const createdId = await blogRepository.create({
+      ...input,
+      createdAt: new Date().toISOString(),
+      isMembership: false,
+    });
+
+    const createdBlog = await blogRepository.findById(createdId);
+
+    if (createdBlog === null) {
+      throw new Error('400');
+    }
+
+    return createdBlog;
+  },
+  async update({ blogId, update }: UpdateBlogType): Promise<boolean> {
+    if (!ObjectId.isValid(blogId)) {
+      throw new Error('400');
+    }
+
+    const success = await blogRepository.update({ blogId, update });
+
+    if (!success) {
+      throw new Error('404');
+    }
+
+    return success;
+  },
+  async findAll(): Promise<OutputBlogType[]> {
+    return blogRepository.findAll();
+  },
+  async findById(id: string): Promise<OutputBlogType> {
+    if (!ObjectId.isValid(id)) {
+      throw new Error('400');
+    }
+    const found = await blogRepository.findById(id);
+
+    if (found === null) {
+      throw new Error('404');
+    }
+
+    return found;
+  },
+  async deleteById(id: string): Promise<boolean> {
+    if (!ObjectId.isValid(id)) {
+      throw new Error('400');
+    }
+
+    const success = await blogRepository.deleteById(id);
+
+    if (!success) {
+      throw new Error('404');
+    }
+    return success;
+  },
+};
