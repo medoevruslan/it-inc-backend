e01daf204b10251280ae95af8976a4dd801376c2
MedoevRuslan <mr.r.m3@icloud.com>
2025-03-01 13:55:41 +0200

    feat: add types

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,1 +1,9 @@
 export type Nullable<Type> = null | Type;
+
+export type GetAllQueryParams<Model> = {
+  searchNameTerm: string;
+  sortBy: keyof Model;
+  sortDirection: 'asc' | 'desc';
+  pageNumber: number;
+  pageSize: number;
+};
