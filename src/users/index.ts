4121de72c06fdd99656eddb90345781c49c9af30
MedoevRuslan <mr.r.m3@icloud.com>
2025-03-16 16:29:30 +0200

    feat(users/index): handle get users api call

--- src/users/index.ts
+++ src/users/index.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import { getUsersController } from './getUsersController';
 
 export const usersRouter = Router();
 
-usersRouter.get('/');
+usersRouter.get('/', getUsersController);
 usersRouter.post('/');
 usersRouter.delete('/');
