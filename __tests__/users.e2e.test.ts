947c02ccf64155106d0d27b94b80e21059862668
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-06 18:46:13 +0300

    refac: write test case

--- __tests__/users.e2e.test.ts
+++ __tests__/users.e2e.test.ts
@@ -1,8 +1,9 @@
 import { req, toBase64 } from './test-helpers';
 import { SETTINGS } from '../src/settings';
 import { InputUserType } from '../src/input-output-types/user-types';
-import { ObjectId } from 'mongodb';
-import { setMongoDB } from '../src/db/mongoDb';
+import { runDb, setMongoDB } from '../src/db/mongoDb';
+
+(async () => await runDb(SETTINGS.MONGO_URL))();
 
 describe('tests for /users', () => {
   const codedAuth = toBase64(SETTINGS.ADMIN_AUTH);
@@ -11,17 +12,24 @@ describe('tests for /users', () => {
     await setMongoDB();
     const usersResponse = await req.get(SETTINGS.PATH.USERS).expect(200);
 
-    expect(usersResponse.body.items.length).toBeGreaterThan(0);
+    expect(usersResponse.body.length).toBe(0);
   });
   it('should create new user', async () => {
     await setMongoDB();
-    const user: Partial<InputUserType> = {
+    const newUser: Partial<InputUserType> = {
       login: 'new login',
       email: 'new email',
       password: 'new password',
     };
 
-    const createUserResponse = await req.post(SETTINGS.PATH.USERS).send(user).expect(201);
+    const createUserResponse = await req.post(SETTINGS.PATH.USERS).send(newUser).expect(201);
+
+    expect(createUserResponse.body.login).toBe(newUser.login);
+    expect(createUserResponse.body.email).toBe(newUser.email);
+
+    const usersResponse = await req.get(SETTINGS.PATH.USERS).expect(200);
+
+    expect(usersResponse.body.length).toBe(1);
   });
   it('should not create new user because unauthorized', async () => {});
   it('should delete user', async () => {
