e76723c3c6d1a5f4ccdb02abe68e27aed8f5e963
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-06 14:59:13 +0300

    feat: return stub on create

---
+++ src/service/userService.ts
@@ -0,0 +1,9 @@
+import { InputUserType } from '../input-output-types/user-types';
+
+export const userService = {
+  create(user: InputUserType) {},
+  update() {},
+  findAll() {
+    return { items: [] };
+  },
+};
