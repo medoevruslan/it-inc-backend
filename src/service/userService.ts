1b04a48413b301535a0639af52c1af46aa8c339c
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-06 18:35:46 +0300

    feat: implement create logic

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,9 +1,28 @@
 import { InputUserType } from '../input-output-types/user-types';
+import { HttpStatuses } from '../shared/enums';
+import { userRepository } from '../repository/userRepository';
+import { UserDbType } from '../db/user-db-type';
+import bcrypt from 'bcrypt';
 
 export const userService = {
-  create(user: InputUserType) {},
-  update() {},
-  findAll() {
-    return { items: [] };
+  async create(user: InputUserType) {
+    const { login, password, email } = user;
+    const isAlreadyExists = await userRepository.findByLoginOrEmail(email);
+
+    if (isAlreadyExists) {
+      throw Error(HttpStatuses.BadRequest.toString());
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser: UserDbType = {
+      login,
+      email,
+      password: hashedPassword,
+      createdAt: new Date(),
+    };
+
+    return userRepository.create(newUser);
   },
+  update() {},
 };
