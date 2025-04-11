d2f30a56d9ccde14a501b5fb76df4fd33d3eaeb6
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-11 12:52:34 +0300

    refac(deleteUserController): implement logic

--- src/users/deleteUserController.ts
+++ src/users/deleteUserController.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from 'express';
 import { HttpStatuses } from '../shared/enums';
-export const deleteUserController = (req: Request, res: Response) => {
+import { userService } from '../service/userService';
+export const deleteUserController = async (req: Request<{ id: string }>, res: Response) => {
   try {
-    res.status(HttpStatuses.NoContent).send('deleteUserController'); //some
-  } catch (err) {}
+    const userId = req.params.id;
+    await userService.deleteById(req.params.id);
+    res.status(HttpStatuses.NoContent).send();
+  } catch (err) {
+    const error = err as Error;
+    const errorCode = Number(error.message);
+    if (isFinite(errorCode)) {
+      res.status(errorCode).send();
+    } else {
+      res.status(500).send(error.message);
+    }
+  }
 };
