c7cf33499cf65785c582063e969d6c3c9d8f6440
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-11 20:34:12 +0300

    feat: add method

--- src/repository/userQueryRepository.ts
+++ src/repository/userQueryRepository.ts
@@ -1,10 +1,45 @@
 import { userMapper } from '../mapping/userMapper';
-import { ObjectId } from 'mongodb';
+import { ObjectId, WithId } from 'mongodb';
 import { db } from '../db/mongoDb';
+import { GetAllUsersQueryParams, OutputUserType } from '../input-output-types/user-types';
+import { OutputModelTypeWithInfo } from '../input-output-types/common-types';
+import { UserDbType } from '../db/user-db-type';
 
 export const userQueryRepository = {
-  async findAll() {
-    return await db.getCollections().usersCollection.find().toArray();
+  async findAll(inputFilter: GetAllUsersQueryParams): Promise<OutputModelTypeWithInfo<OutputUserType>> {
+    const { sortDirection, sortBy, pageSize, pageNumber, searchLoginTerm, searchEmailTerm } = inputFilter;
+    let filter = {};
+    if (searchLoginTerm) {
+      filter = { ...filter, login: { $regex: searchLoginTerm, $options: 'i' } };
+    }
+
+    if (searchEmailTerm) {
+      filter = { ...filter, email: { $regex: searchEmailTerm, $options: 'i' } };
+    }
+
+    const convertedPageSize = Number(pageSize);
+
+    const skip = (Number(pageNumber) - 1) * convertedPageSize;
+
+    // Execute queries in parallel for better performance
+    const [totalCount, users]: [number, WithId<UserDbType>[]] = await Promise.all([
+      db.getCollections().usersCollection.countDocuments(filter), // Fetch total count
+      db
+        .getCollections()
+        .usersCollection.find(filter)
+        .sort({ [sortBy]: sortDirection === 'asc' ? 1 : -1 })
+        .skip(skip)
+        .limit(convertedPageSize)
+        .toArray(),
+    ]);
+
+    return {
+      pagesCount: Math.ceil(totalCount / convertedPageSize),
+      page: Number(pageNumber),
+      pageSize: convertedPageSize,
+      totalCount: totalCount,
+      items: users.map(userMapper.mapUserToOutputType),
+    };
   },
   async findById(id: string) {
     const result = await db.getCollections().usersCollection.findOne({ _id: new ObjectId(id) });
