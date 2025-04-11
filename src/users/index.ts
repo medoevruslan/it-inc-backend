4819e44d84e61bcb77f639d498bd0ffc7f492c97
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-11 23:26:34 +0300

    feat: create body validator for users route

--- src/users/index.ts
+++ src/users/index.ts
@@ -4,9 +4,10 @@ import { createUserController } from './createUserController';
 import { deleteUserController } from './deleteUserController';
 import { authMiddleware } from '../middlewares';
 import { userQueryValidator } from '../validation/userQueryValidator';
+import { userBodyValidator } from '../validation/userBodyValidator';
 
 export const usersRouter = Router();
 
 usersRouter.get('/', userQueryValidator, authMiddleware, getUsersController);
-usersRouter.post('/', authMiddleware, createUserController);
+usersRouter.post('/', userBodyValidator, authMiddleware, createUserController);
 usersRouter.delete('/:id', authMiddleware, deleteUserController);
