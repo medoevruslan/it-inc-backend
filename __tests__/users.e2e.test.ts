68f65eb67a81084e3ae1e76a00e5b22ea02a0ede
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-11 12:53:36 +0300

    feat: add test cases

--- __tests__/users.e2e.test.ts
+++ __tests__/users.e2e.test.ts
@@ -2,6 +2,7 @@ import { req, toBase64 } from './test-helpers';
 import { SETTINGS } from '../src/settings';
 import { InputUserType } from '../src/input-output-types/user-types';
 import { db } from '../src/db/mongoDb';
+import exp = require('node:constants');
 
 (async () => await db.run(SETTINGS.MONGO_URL))();
 
@@ -22,7 +23,11 @@ describe('tests for /users', () => {
       password: 'new password',
     };
 
-    const createUserResponse = await req.post(SETTINGS.PATH.USERS).send(newUser).expect(201);
+    const createUserResponse = await req
+      .post(SETTINGS.PATH.USERS)
+      .set('Authorization', `Basic ${codedAuth}`)
+      .send(newUser)
+      .expect(201);
 
     expect(createUserResponse.body.login).toBe(newUser.login);
     expect(createUserResponse.body.email).toBe(newUser.email);
@@ -31,7 +36,45 @@ describe('tests for /users', () => {
 
     expect(usersResponse.body.length).toBe(1);
   });
-  it('should not create new user because unauthorized', async () => {});
+  it('should not create new user because unauthorized', async () => {
+    await db.dropCollections();
+    const newUser: Partial<InputUserType> = {
+      login: 'new login',
+      email: 'new email',
+      password: 'new password',
+    };
+
+    const createUserResponse = await req.post(SETTINGS.PATH.USERS).send(newUser).expect(401);
+  });
+  it('should not create new user because already exists', async () => {
+    await db.dropCollections();
+    const newUser: Partial<InputUserType> = {
+      login: 'new login',
+      email: 'new email',
+      password: 'new password',
+    };
+
+    const existedUser: Partial<InputUserType> = {
+      login: 'new login',
+      email: 'new email',
+      password: 'new password',
+    };
+
+    const createUserResponse1 = await req
+      .post(SETTINGS.PATH.USERS)
+      .set('Authorization', `Basic ${codedAuth}`)
+      .send(newUser)
+      .expect(201);
+
+    const createUserResponse2 = await req
+      .post(SETTINGS.PATH.USERS)
+      .set('Authorization', `Basic ${codedAuth}`)
+      .send(existedUser)
+      .expect(400);
+
+    expect(createUserResponse2.body.errorsMessages.length).toBe(1);
+    expect(createUserResponse2.body.errorsMessages[0]).toEqual({ field: 'email', message: 'email should be unique' });
+  });
   it('should delete user', async () => {
     await db.dropCollections();
     const user: Partial<InputUserType> = {
@@ -40,9 +83,35 @@ describe('tests for /users', () => {
       password: 'new password',
     };
 
-    const createUserResponse = await req.post(SETTINGS.PATH.USERS).send(user).expect(201);
+    const createUserResponse = await req
+      .post(SETTINGS.PATH.USERS)
+      .send(user)
+      .set('Authorization', `Basic ${codedAuth}`)
+      .expect(201);
 
-    const deleteUserResponse = await req.delete(`${SETTINGS.PATH.USERS}/${createUserResponse.body.userId}`).expect(204);
+    const deleteUserResponse = await req
+      .delete(`${SETTINGS.PATH.USERS}/${createUserResponse.body.id}`)
+      .set('Authorization', `Basic ${codedAuth}`)
+      .expect(204);
+
+    const getUsersResponse = await req.get(SETTINGS.PATH.USERS).expect(200);
+
+    expect(getUsersResponse.body.length).toBe(0);
   });
-  it('should not delete user because unauthorized', async () => {});
+  it('should not delete user because unauthorized', async () => {
+    await db.dropCollections();
+    const user: Partial<InputUserType> = {
+      login: 'new login',
+      email: 'new email',
+      password: 'new password',
+    };
+
+    const createUserResponse = await req
+      .post(SETTINGS.PATH.USERS)
+      .send(user)
+      .set('Authorization', `Basic ${codedAuth}`)
+      .expect(201);
+
+    const deleteUserResponse = await req.delete(`${SETTINGS.PATH.USERS}/${createUserResponse.body.id}`).expect(401);
+  });
 });
