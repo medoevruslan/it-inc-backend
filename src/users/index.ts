90f3f66cb72568fb4b145f5893ea301e117a0a09
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-11 12:52:52 +0300

    feat: add auth middleware

--- src/users/index.ts
+++ src/users/index.ts
@@ -2,9 +2,10 @@ import { Router } from 'express';
 import { getUsersController } from './getUsersController';
 import { createUserController } from './createUserController';
 import { deleteUserController } from './deleteUserController';
+import { authMiddleware } from '../middlewares';
 
 export const usersRouter = Router();
 
 usersRouter.get('/', getUsersController);
-usersRouter.post('/', createUserController);
-usersRouter.delete('/:id', deleteUserController);
+usersRouter.post('/', authMiddleware, createUserController);
+usersRouter.delete('/:id', authMiddleware, deleteUserController);
