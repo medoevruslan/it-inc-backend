a81654b5240205604502e14c3068048653972ba6
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-12 11:49:16 +0300

    feat: add validation

--- src/users/index.ts
+++ src/users/index.ts
@@ -2,12 +2,12 @@ import { Router } from 'express';
 import { getUsersController } from './getUsersController';
 import { createUserController } from './createUserController';
 import { deleteUserController } from './deleteUserController';
-import { authMiddleware } from '../middlewares';
+import { authMiddleware, validationErrorMiddleware } from '../middlewares';
 import { userQueryValidator } from '../validation/userQueryValidator';
 import { userBodyValidator } from '../validation/userBodyValidator';
 
 export const usersRouter = Router();
 
 usersRouter.get('/', userQueryValidator, authMiddleware, getUsersController);
-usersRouter.post('/', userBodyValidator, authMiddleware, createUserController);
+usersRouter.post('/', userBodyValidator, authMiddleware, validationErrorMiddleware, createUserController);
 usersRouter.delete('/:id', authMiddleware, deleteUserController);
