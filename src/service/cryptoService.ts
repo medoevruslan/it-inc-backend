1a13106e36dec98ba3ca943f3140cb42bebe9ac6
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-12 16:34:23 +0300

    refac: make param set by default

--- src/service/cryptoService.ts
+++ src/service/cryptoService.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 
 export const cryptoService = {
-  async genHash(password: string, salt: number | string) {
+  async genHash(password: string, salt: number | string = 10) {
     return await bcrypt.hash(password, salt);
   },
   async genSalt(rounds = 10) {
