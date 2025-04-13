40b9255a6e75ab30fe1dec16f253d1660ba0526a
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-13 16:43:34 +0300

    fix: fix type

--- src/input-output-types/user-types.ts
+++ src/input-output-types/user-types.ts
@@ -3,7 +3,7 @@ import { UserDbType } from '../db/user-db-type';
 export type InputUserType = Omit<UserDbType, 'createdAt'>;
 export type OutputUserType = Omit<UserType, 'password'>;
 
-export type UserType = Omit<UserDbType, '_id'> & { id: string };
+export type UserType = UserDbType & { id: string };
 
 export type GetAllUsersQueryParams = {
   searchLoginTerm: string;
