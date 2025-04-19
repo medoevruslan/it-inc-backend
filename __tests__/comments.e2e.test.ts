c236045421543434ee1c8324d05ab3a1115fb751
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-19 11:40:14 +0300

    feat: add test cases

--- __tests__/comments.e2e.test.ts
+++ __tests__/comments.e2e.test.ts
@@ -71,6 +71,88 @@ describe('test /comments', () => {
         .expect(HttpStatuses.Unauthorized);
     });
   });
+  describe('update comments', () => {
+    it('should update existent comment', async () => {
+      await db.dropCollections();
+      await db.seed({ comments: [comment1] });
+
+      const newUser = await addUser(codedAuth);
+
+      const loginResponse = await req
+        .post(`${SETTINGS.PATH.AUTH}/login`)
+        .send({ loginOrEmail: newUser.login, password: newUser.password })
+        .expect(200);
+
+      const commentUpdate = { content: 'a'.repeat(20) };
+      const updateResponse = await req
+        .put(`${SETTINGS.PATH.COMMENTS}/${comment1._id.toString()}`)
+        .set('Authorization', `Bearer ${loginResponse.body.accessToken}`)
+        .send(commentUpdate)
+        .expect(204);
+
+      const commentsResponse = await req
+        .get(`${SETTINGS.PATH.COMMENTS}/${comment1._id.toString()}`)
+        .set('Authorization', `Bearer ${loginResponse.body.accessToken}`)
+        .expect(200);
+
+      expect(commentsResponse.body.content).not.toBe(comment1.content);
+      expect(commentsResponse.body.content).toBe(commentUpdate.content);
+    });
+    it('should not update existent comment because to short', async () => {
+      await db.dropCollections();
+      await db.seed({ comments: [comment1] });
+
+      const newUser = await addUser(codedAuth);
+
+      const loginResponse = await req
+        .post(`${SETTINGS.PATH.AUTH}/login`)
+        .send({ loginOrEmail: newUser.login, password: newUser.password })
+        .expect(200);
+
+      const commentUpdate = { content: 'a'.repeat(19) };
+      const updateResponse = await req
+        .put(`${SETTINGS.PATH.COMMENTS}/${comment1._id.toString()}`)
+        .set('Authorization', `Bearer ${loginResponse.body.accessToken}`)
+        .send(commentUpdate)
+        .expect(400);
+
+      expect(updateResponse.body.errorsMessages[0]).toEqual({
+        field: 'content',
+        message: 'name should be less than 300 chars and more than 20',
+      });
+    });
+    it('should not update existent comment because to long', async () => {
+      await db.dropCollections();
+      await db.seed({ comments: [comment1] });
+
+      const newUser = await addUser(codedAuth);
+
+      const loginResponse = await req
+        .post(`${SETTINGS.PATH.AUTH}/login`)
+        .send({ loginOrEmail: newUser.login, password: newUser.password })
+        .expect(200);
+
+      const commentUpdate = { content: 'a'.repeat(301) };
+      const updateResponse = await req
+        .put(`${SETTINGS.PATH.COMMENTS}/${comment1._id.toString()}`)
+        .set('Authorization', `Bearer ${loginResponse.body.accessToken}`)
+        .send(commentUpdate)
+        .expect(400);
+
+      expect(updateResponse.body.errorsMessages[0]).toEqual({
+        field: 'content',
+        message: 'name should be less than 300 chars and more than 20',
+      });
+    });
+    it('should not update because unauthorized', async () => {
+      await db.dropCollections();
+      await db.seed({ comments: [comment1] });
+
+      const deleteCommentsResponse = await req
+        .put(`${SETTINGS.PATH.COMMENTS}/${comment1._id.toString()}`)
+        .expect(HttpStatuses.Unauthorized);
+    });
+  });
 });
 
 const addUser = async (auth: string) => {
