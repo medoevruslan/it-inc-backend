3f0af6da8c48520761f76c83ba5acf84fc71cd6a
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-19 11:40:01 +0300

    feat: add error handling

--- src/comments/deleteCommentsController.ts
+++ src/comments/deleteCommentsController.ts
@@ -5,5 +5,13 @@ export const deleteCommentsController = async (req: Request<{ commentId: string 
   try {
     await commentService.delete(req.params.commentId);
     res.sendStatus(HttpStatuses.NoContent);
-  } catch (err: unknown) {}
+  } catch (err: unknown) {
+    const error = err as Error;
+    const errorCode = Number(error.message);
+    if (isFinite(errorCode)) {
+      res.status(errorCode).send();
+    } else {
+      res.status(500).send(error.message);
+    }
+  }
 };
