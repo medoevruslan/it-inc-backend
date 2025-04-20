a7517244261cbec42587047f87cf42e1b0348d59
MedoevRuslan <mr.r.m3@icloud.com>
2025-04-20 13:08:28 +0300

    feat: add new type

--- src/input-output-types/comment-types.ts
+++ src/input-output-types/comment-types.ts
@@ -2,4 +2,5 @@ import { CommentDbType } from '../db/comment-db-type';
 
 export type CommentType = CommentDbType & { id: string };
 export type CommentUpdateType = { commentId: string; update: { content: string } };
+export type CommentInputType = { userId: string; postId: string; content: string };
 export type OutputCommentType = CommentType;
