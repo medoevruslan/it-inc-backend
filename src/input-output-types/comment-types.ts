7e32be798f2f1a4729758dfc11c590cf1c7efaf0
MedoevRuslan <mr.r.m3@icloud.com>
2025-05-08 11:09:22 +0300

    refac: change type

--- src/input-output-types/comment-types.ts
+++ src/input-output-types/comment-types.ts
@@ -3,4 +3,4 @@ import { CommentDbType } from '../db/comment-db-type';
 export type CommentType = CommentDbType & { id: string };
 export type CommentUpdateType = { commentId: string; update: { content: string } };
 export type CommentInputType = { userId: string; postId: string; content: string };
-export type OutputCommentType = CommentType;
+export type OutputCommentType = Omit<CommentType, 'postId'>;
