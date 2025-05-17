6ac8c76ebca6dbe8bb57afef0105edf0ecf22460
MedoevRuslan <mr.r.m3@icloud.com>
2025-05-17 12:29:57 +0300

    feat: create manager

--- src/managers/emailManager.ts
+++ src/managers/emailManager.ts
@@ -1,3 +1,28 @@
+import nodemailer from 'nodemailer';
+import { BASE_URL, SETTINGS } from '../settings';
 
 
-export const emailManager = {}
+export const emailManager = {
+  async sendEmailConfirmation(userData: { email: string, verificationCode: string }) {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: 'rus.terra.86@gmail.com',
+        clientId: SETTINGS.GOOGLE.CLIENT_ID,
+        clientSecret: SETTINGS.GOOGLE.CLIENT_SECRET,
+        refreshToken: SETTINGS.GOOGLE.REFRESH_TOKEN,
+      },
+    });
+
+
+    const sendMailInfo = await transporter.sendMail({
+      from: 'Blog service <rus.terra.86@gmail.com>',
+      to: userData.email,
+      subject: 'Email verification',
+      html: `<b>Please verify your email by clicking the <a href=http://localhost:3000${BASE_URL}?code=${userData.verificationCode}>link</a></b>`,
+    });
+
+    return sendMailInfo;
+  }
+}
