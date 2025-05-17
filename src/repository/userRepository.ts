bf914c6048f1147fdc65ecd76c8583282746be83
MedoevRuslan <mr.r.m3@icloud.com>
2025-05-17 12:30:41 +0300

    feat: add methods

--- src/repository/userRepository.ts
+++ src/repository/userRepository.ts
@@ -1,6 +1,7 @@
 import { UserDbType, UserType } from '../db/user-db-type';
 import { db } from '../db/mongoDb';
 import { ObjectId } from 'mongodb';
+import { userMapper } from '../mapping/userMapper';
 
 export const userRepository = {
   async create(user: UserType) {
@@ -16,8 +17,22 @@ export const userRepository = {
       .usersCollection.findOne({ $or: [{ 'accountData.email': loginOrEmail }, { 'accountData.login': loginOrEmail }] });
   },
 
+  async findByConfirmationCode(code: string) {
+    return await db.getCollections().usersCollection.findOne({ 'accountData.confirmationCode': code });
+  },
+
+  async findById(id: string) {
+    const result = await db.getCollections().usersCollection.findOne({ _id: new ObjectId(id) });
+    return result ? userMapper.mapUserToOutputType(result) : null;
+  },
+
   async deleteById(userId: string) {
     const result = await db.getCollections().usersCollection.deleteOne({ _id: new ObjectId(userId) });
     return result.deletedCount === 1;
   },
+
+  async update(userId: string, update: Partial<UserType>) {
+    const result = await db.getCollections().usersCollection.updateOne({ _id: new ObjectId(userId)}, { $set: update });
+    return result.matchedCount === 1;
+  }
 };
