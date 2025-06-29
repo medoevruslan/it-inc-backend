ab39d49cec7bccbf41127c765fa701e647b69391
MedoevRuslan <mr.r.m3@icloud.com>
2025-06-29 13:39:43 +0300

    feat: add dependency

--- src/composition-root.ts
+++ src/composition-root.ts
@@ -5,6 +5,7 @@ import { UserService } from './service/userService';
 import { AuthService } from './service/authService';
 import { EmailManager } from './managers/emailManager';
 import { RefreshTokenBlockedRepository } from './repository/refreshTokenBlockedRepository';
+import { DeviceAuthSessionsRepository } from './repository/deviceAuthSessionsRepository';
 
 const userRepository = new UserRepository();
 const emailAdapter = new EmailAdapter();
@@ -14,6 +15,7 @@ export const userService = new UserService(userRepository)
 export const jwtService = new JwtService();
 
 const refreshTokensBlockedRepository = new RefreshTokenBlockedRepository()
+const deviceAuthSessionsRepository = new DeviceAuthSessionsRepository()
 
 
-export const authService = new AuthService(emailManager, userService, userRepository, jwtService, refreshTokensBlockedRepository)
+export const authService = new AuthService(emailManager, userService, userRepository, jwtService, refreshTokensBlockedRepository, deviceAuthSessionsRepository)
