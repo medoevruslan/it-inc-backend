035873302f7c287b8cb342c343c086702466f0f1
MedoevRuslan <mr.r.m3@icloud.com>
2025-06-30 16:14:09 +0300

    feat: add new methods

--- src/service/deviceSessionsService.ts
+++ src/service/deviceSessionsService.ts
@@ -2,13 +2,23 @@ import { DeviceAuthSessionsRepository } from '../repository/deviceAuthSessionsRe
 import { sessionsMapper } from '../mapping/sessionsMapper';
 import { JwtService } from './jwtService';
 import { ResultStatus } from '../shared/enums';
+import { DeviceAuthSessionsDbType, DeviceAuthSessionsUpdateType } from '../db/device-auth-sessions-db-type';
 
 export class DeviceSessionsService {
 
   public constructor(protected deviceAuthSessionsRepository: DeviceAuthSessionsRepository, protected jwtService: JwtService) {
   }
 
-  public async findSessionsByUserId({ refreshToken }: { refreshToken: string }) {
+  public async create(data: DeviceAuthSessionsDbType) {
+    const { deviceId, iat, deviceName, ip, exp, userId } = data
+    return this.deviceAuthSessionsRepository.add({ deviceId, userId, iat, deviceName, ip, exp});
+  }
+
+  public async update(data: DeviceAuthSessionsUpdateType ) {
+    return this.deviceAuthSessionsRepository.update(data)
+  }
+
+  public async findSessionsByTokenPayload(refreshToken: string) {
 
     const tokenResult = await this.jwtService.verifyToken<{ deviceId: string, iat: number }>(refreshToken);
 
@@ -22,19 +32,35 @@ export class DeviceSessionsService {
       };
     }
 
-    const res = await this.deviceAuthSessionsRepository.findByDeviceIdAndIat(tokenResult.deviceId, tokenResult.iat)
+    const deviceSessionResult = await this.findSessionByDeviceIdAndIat(tokenResult.deviceId, tokenResult.iat)
+
+    if (deviceSessionResult.status !== ResultStatus.Success || !deviceSessionResult.data) {
+      return deviceSessionResult
+    }
+
+    const result = await this.deviceAuthSessionsRepository.findByUserId(deviceSessionResult.data.userId);
+    const mapped = result.map(sessionsMapper.mapSessionsToOutputType);
+
+    return mapped
+  }
+
+  public async findSessionByDeviceIdAndIat(deviceId: string, iat: number) {
+    const res = await this.deviceAuthSessionsRepository.findByDeviceIdAndIat(deviceId, iat)
 
     if (!res) {
-      console.log(`findSessionsByUserId findByDeviceIdAndIat bad result: ${res}`)
+      console.log(`findSessionByDeviceIdAndIat bad result: ${res}`)
       return {
         status: ResultStatus.BadRequest,
-        errorMessage: `Token is not valid`,
-        extensions: [{ field: 'token', message: 'Token is not valid' }],
+        errorMessage: `no session found for device: ${deviceId}`,
+        extensions: [],
         data: null,
       };
     }
 
-    const result = await this.deviceAuthSessionsRepository.findByUserId(res.userId);
-    return result.map(sessionsMapper.mapSessionsToOutputType);
+    return {
+      status: ResultStatus.Success,
+      extensions: [],
+      data: res,
+    }
   }
 }
