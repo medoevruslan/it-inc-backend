1c85ec41053cfa923d165b49e8cbf390df84e099
MedoevRuslan <mr.r.m3@icloud.com>
2025-07-18 23:12:34 +0300

    feat: bind controllers to routes

--- src/device-sessions/index.ts
+++ src/device-sessions/index.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
-import { getActiveSessionsByUserId } from './getActiveSessionsByUserId';
+import { getActiveSessionsByUserIdController } from './getActiveSessionsByUserIdController';
+import { deleteActiveSessionsExceptForActiveController } from './deleteActiveSessionsExceptForActiveController';
+import { deleteUserSessionByDeviceId } from './deleteUserSessionByDeviceId';
+import { refreshTokenGuard } from '../middlewares/guard/refreshTokenGuard';
 
 
 export const securityRouter = Router()
 
-securityRouter.get('/devices/', getActiveSessionsByUserId)
-securityRouter.delete('/devices/')
-securityRouter.delete('/devices/{:deviceId}')
+// TODO: add middleware for refresh token verification and add userId & deviceId to req.
+
+securityRouter.get('/devices/', refreshTokenGuard, getActiveSessionsByUserIdController)
+securityRouter.delete('/devices/', refreshTokenGuard, deleteActiveSessionsExceptForActiveController)
+securityRouter.delete('/devices/:deviceId',refreshTokenGuard, deleteUserSessionByDeviceId)
 
 
