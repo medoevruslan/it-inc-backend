be53b9af625162e9772c495e0eb1c400da08cc9c
MedoevRuslan <mr.r.m3@icloud.com>
2025-07-20 12:07:56 +0300

    refac(deleteUserSessionByDeviceIdController): refactor logic

--- src/device-sessions/index.ts
+++ src/device-sessions/index.ts
@@ -1,16 +1,14 @@
 import { Router } from 'express';
 import { getActiveSessionsByUserIdController } from './getActiveSessionsByUserIdController';
 import { deleteActiveSessionsExceptForActiveController } from './deleteActiveSessionsExceptForActiveController';
-import { deleteUserSessionByDeviceId } from './deleteUserSessionByDeviceId';
+import { deleteUserSessionByDeviceIdController } from './deleteUserSessionByDeviceIdController';
 import { refreshTokenGuard } from '../middlewares/guard/refreshTokenGuard';
 
 
 export const securityRouter = Router()
 
-// TODO: add middleware for refresh token verification and add userId & deviceId to req.
-
 securityRouter.get('/devices/', refreshTokenGuard, getActiveSessionsByUserIdController)
 securityRouter.delete('/devices/', refreshTokenGuard, deleteActiveSessionsExceptForActiveController)
-securityRouter.delete('/devices/:deviceId',refreshTokenGuard, deleteUserSessionByDeviceId)
+securityRouter.delete('/devices/:deviceId',refreshTokenGuard, deleteUserSessionByDeviceIdController)
 
 
