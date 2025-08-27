39b6a7c083f73d538cb6d500cff3a7ebef91c3e0
MedoevRuslan <mr.r.m3@icloud.com>
2025-08-27 13:34:54 +0200

    refac(comment-types): refactor types

--- src/input-output-types/comment-types.ts
+++ src/input-output-types/comment-types.ts
@@ -1,7 +1,20 @@
 import { CommentDbType } from '../db/comment-db-type';
+import { LikeType } from '../shared/enums';
+import { ObjectId } from 'mongodb';
 
 export type CommentType = Omit<CommentDbType, '_id' | 'createdAt'>;
 export type CommentUpdateType = { commentId: string; update: Partial<CommentDbType> };
 export type CommentInputType = { userId: string; postId: string; content: string };
-export type CommentOutputType = Omit<CommentType, 'postId'> & { id: string, createdAt: string }
+export type CommentOutputType = Omit<CommentType, 'postId'> & { id: string, createdAt: string, likesInfo: LikesInfo }
 
+export type LikesInfo = {
+  likesCount: number,
+  dislikesCount: number,
+  myStatus: LikeType
+}
+
+export type LikesAggregationResult = {
+  _id: ObjectId;
+  likesCount: number;
+  dislikesCount: number;
+};
